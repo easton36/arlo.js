72e132bd022c95f98fb912a525d4f348fd00eabd
Easton <easton.schram@gmail.com>
2022-07-09 05:28:20 -0500

    Update tsconfig.json

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Basestation = exports.Camera = void 0;
-var Client_1 = require("./src/Client");
-var Camera_1 = require("./src/Camera");
-exports.Camera = Camera_1["default"];
-var Basestation_1 = require("./src/Basestation");
-exports.Basestation = Basestation_1["default"];
-exports["default"] = Client_1["default"];
---
+++ src/utils/assert.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+function assert(value, error) {
+    if (value === undefined || value === null || value === false) {
+        throw new Error(error);
+    }
+}
+exports["default"] = assert;
+;
